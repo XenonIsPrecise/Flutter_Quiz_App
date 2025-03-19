229fa76f5422182331168500e57472a1db9693ba
Sujal Acharya <sujalacharya567@gmail.com>
2025-03-19 14:16:43 -0300

    Some Changes

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,7 +9,13 @@ class StartScreen extends StatelessWidget {
         child: Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset('assets/images/quiz-logo.png', width: 200),
+        Image.asset(
+          'assets/images/quiz-logo.png',
+          width: 200,
+          color: const Color.fromARGB(150, 176, 250, 195),
+        ), // Opacity(
+        //     opacity: 0.5,
+        //     child: Image.asset('assets/images/quiz-logo.png', width: 200)),
         const SizedBox(height: 20),
         const Text(
           'Learning flutter the fun way',
@@ -19,10 +25,11 @@ class StartScreen extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 20),
-        OutlinedButton(
+        OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: const Text('Start Quiz')),
+            icon: const Icon(Icons.play_circle_fill_sharp),
+            label: const Text('Start Quiz')),
       ],
     ));
   }
