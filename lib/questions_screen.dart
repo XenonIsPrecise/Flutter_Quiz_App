4c2a28984b7357a2f60ec1527a6b8f9d81f85087
Sujal Acharya <sujalacharya567@gmail.com>
2025-03-25 14:23:04 -0300

    Problem with questions not changing

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,22 +10,44 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  answerQuestion() {
+    setState(() {
+      if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex += 1;
+        debugPrint("Current Question Index: $currentQuestionIndex");
+      } else {
+        debugPrint("Quiz Finished!");
+      }
+    });
+  }
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[currentQuestionIndex];
+
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Questions",
-            style: TextStyle(fontSize: 20),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          AnswerButton(answerText: "answerText", onTap: () {})
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((item) {
+              return AnswerButton(answerText: item, onTap: answerQuestion);
+            })
+          ],
+        ),
       ),
     );
   }
